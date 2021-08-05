ab4c0130eebf93b11f03f1fa0b40cd41728f1140
Athena Chen <achen70@jhu.edu>
2021-08-05 16:46:02 -0500

    Added function to calculate proportion of reads

--- R/summaries.R
+++ R/summaries.R
@@ -11,7 +11,7 @@ NULL
 #' @param withDimnames logical; if true, the vector names are the sample names;
 #' otherwise the vector is unnamed.
 #'
-#' @return a named numeric vector. The length of the vector is equal to the
+#' @return a (named) numeric vector. The length of the vector is equal to the
 #' number of samples.
 #'
 #' @examples
@@ -25,4 +25,32 @@ NULL
 librarySize <- function(object, ..., withDimnames = TRUE){
   sums <- colSums(counts(object,...))
   if(withDimnames){ sums } else { unname(sums) }
+}
+
+#' Proportion of sample reads
+#'
+#' This function calculates the proportion of total sample reads pulled by
+#' each peptide.
+#'
+#' @param object \linkS4class{PhIPData} object
+#' @param withDimnames logical; if true return a matrix with the same dimension
+#' names as the original object.
+#'
+#' @return A (named) numeric matrix with the same dimensions as the function
+#' input. Matrix values are between 0 and 1.
+#'
+#' @examples
+#' example("PhIPData")
+#' propReads(phip_obj)
+#'
+#' ## Return an unnamed matrix
+#' propReads(phip_obj, withDimnames = FALSE)
+#'
+#' @export
+propReads <- function(object, withDimnames = TRUE){
+    n <- librarySize(object, withDimnames = FALSE)
+    n_matrix <- matrix(rep(n, nrow(object)), nrow = nrow(object), byrow = TRUE)
+    counts_matrix <- as.matrix(PhIPData::counts(object))
+    prop_matrix <- counts_matrix/n_matrix
+    if(withDimnames){ prop_matrix } else { unname(prop_matrix) }
 }
--- tests/testthat/test-summaries.R
+++ tests/testthat/test-summaries.R
@@ -7,3 +7,11 @@ test_that("library sizes are accurately calculated", {
   expect_equal(librarySize(phip_obj, withDimnames = FALSE),
                unname(colSums(counts)))
 })
+
+test_that("proportion of reads pulled are accurately calculated", {
+    manual_prop <- counts/matrix(rep(colSums(counts), nrow(counts)),
+                                 nrow = nrow(counts), byrow = TRUE)
+    expect_equal(propReads(phip_obj), manual_prop)
+    expect_equal(propReads(phip_obj, withDimnames = FALSE),
+                 unname(manual_prop))
+})
