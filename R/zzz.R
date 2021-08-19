4bb4bec8bff90dd723068ee493a6e318ee6a5c70
Athena Chen <achen70@jhu.edu>
2021-08-19 15:04:32 -0500

    Corrected bug in onLoad that did not make assignments to pkg environment

--- R/zzz.R
+++ R/zzz.R
@@ -1,20 +1,30 @@
 pkg_env <- new.env()
 
+.createDefaults <- function() {
+    assign("BEADS_NAME", "beads", envir = pkg_env)
+    assign("ALIAS_PATH",
+        system.file(package = "PhIPData", "extdata/alias.rda"),
+        envir = pkg_env
+    )
+    assign("PHIP_LIBRARY_PATH",
+        system.file(package = "PhIPData", "libraries"),
+        envir = pkg_env
+    )
+    save(
+        list = c("BEADS_NAME", "ALIAS_PATH", "PHIP_LIBRARY_PATH"),
+        file = paste0(
+            system.file(package = "PhIPData", "extdata"),
+            "/defaults.rda"
+        ),
+        envir = pkg_env
+    )
+}
+
 .onLoad <- function(libname, pkgname) {
     # Build default variables by system if file does not exists
     extdata_path <- system.file(package = "PhIPData", "extdata")
     if (system.file(package = "PhIPData", "extdata/defaults.rda") == "") {
-        BEADS_NAME <- "beads"
-        ALIAS_PATH <- system.file(package = "PhIPData", "extdata/alias.rda")
-        PHIP_LIBRARY_PATH <- system.file(package = "PhIPData", "libraries")
-
-        save(
-            list = c("BEADS_NAME", "ALIAS_PATH", "PHIP_LIBRARY_PATH"),
-            file = paste0(
-                system.file(package = "PhIPData", "extdata"),
-                "/defaults.rda"
-            )
-        )
+        .createDefaults()
     }
     load(system.file(package = "PhIPData", "extdata/defaults.rda"),
         envir = pkg_env
@@ -22,17 +32,7 @@ pkg_env <- new.env()
 
     ## Check if defaults info is valid, if not, rebuild
     if (any(!file.exists(c(pkg_env$ALIAS_PATH, pkg_env$PHIP_LIBRARY_PATH)))) {
-        BEADS_NAME <- "beads"
-        ALIAS_PATH <- system.file(package = "PhIPData", "extdata/alias.rda")
-        PHIP_LIBRARY_PATH <- system.file(package = "PhIPData", "libraries")
-
-        save(
-            list = c("BEADS_NAME", "ALIAS_PATH", "PHIP_LIBRARY_PATH"),
-            file = paste0(
-                system.file(package = "PhIPData", "extdata"),
-                "/defaults.rda"
-            )
-        )
+        .createDefaults()
     }
     load(get("ALIAS_PATH", envir = pkg_env), envir = pkg_env)
 }
